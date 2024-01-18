cc049cb576f6622a04c193eca64d4598ca1a46c6
philipwanjiku <muhorophilip7@gmail.com>
2024-01-18 14:06:08 +0300

    Completed Assignment

---
+++ app/alembic/versions/7b83f9cb65f0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: 7b83f9cb65f0
+Revises: 
+Create Date: 2024-01-18 13:57:28.803590
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7b83f9cb65f0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
